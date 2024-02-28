6fe9005691dec2b5a5fea6224e020489a33b85df
ryosuke-horie <ryosuke03140@gmail.com>
2024-02-28 21:41:32 +0900

    会員情報のリンクの開閉を実装

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { Link } from "@inertiajs/vue3";
+import { ref } from "vue";
 
 defineProps({
     user: {
@@ -7,6 +8,12 @@ defineProps({
         required: true,
     },
 });
+
+// アコーディオン開閉用の変数と関数
+const isOpen = ref(false);
+const toggleAccordion = () => {
+    isOpen.value = !isOpen.value;
+};
 </script>
 
 <template>
@@ -53,10 +60,10 @@ defineProps({
                     data-hs-accordion-always-open
                 >
                     <ul class="space-y-1.5">
-                        <!-- アコーディオン付きのサンプル -->
                         <li class="hs-accordion" id="users-accordion">
                             <button
                                 type="button"
+                                @click="toggleAccordion"
                                 class="hs-accordion-toggle hs-accordion-active:text-blue-600 hs-accordion-active:hover:bg-transparent w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-slate-700 rounded-lg hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-900 dark:text-slate-400 dark:hover:text-slate-300 dark:hs-accordion-active:text-white dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
                             >
                                 <svg
@@ -114,7 +121,8 @@ defineProps({
 
                             <div
                                 id="users-accordion"
-                                class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300 hidden"
+                                class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
+                                v-show="isOpen"
                             >
                                 <ul
                                     class="hs-accordion-group ps-3 pt-2"
@@ -124,11 +132,11 @@ defineProps({
                                         class="hs-accordion"
                                         id="users-accordion-sub-1"
                                     >
-                                        <button
-                                            type="button"
+                                        <Link
+                                            href="/members"
                                             class="hs-accordion-toggle hs-accordion-active:text-blue-600 hs-accordion-active:hover:bg-transparent w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-slate-700 rounded-lg hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-900 dark:text-slate-400 dark:hover:text-slate-300 dark:hs-accordion-active:text-white dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
                                         >
-                                            Sub Menu 1
+                                            会員情報一覧
 
                                             <svg
                                                 class="hs-accordion-active:block ms-auto hidden size-4 text-gray-600 group-hover:text-gray-500 dark:text-gray-400"
@@ -159,23 +167,7 @@ defineProps({
                                             >
                                                 <path d="m6 9 6 6 6-6" />
                                             </svg>
-                                        </button>
-
-                                        <div
-                                            id="users-accordion-sub-1"
-                                            class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300 hidden"
-                                        >
-                                            <ul class="pt-2 ps-2">
-                                                <li>
-                                                    <Link
-                                                        class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-slate-700 rounded-lg hover:bg-gray-100 dark:bg-gray-800 dark:text-slate-400 dark:hover:text-slate-300 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                                                        href="/members"
-                                                    >
-                                                        会員情報一覧
-                                                    </Link>
-                                                </li>
-                                            </ul>
-                                        </div>
+                                        </Link>
                                     </li>
                                 </ul>
                             </div>
