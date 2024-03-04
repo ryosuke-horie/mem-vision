61d9308860398bb84702fbd28abc0e5da7a2eaa8
ryosuke-horie <ryosuke03140@gmail.com>
2024-03-05 08:43:31 +0900

    memoを追加

--- resources/js/Pages/Members/Create.vue
+++ resources/js/Pages/Members/Create.vue
@@ -26,6 +26,7 @@ const firstName = ref("");
 const email = ref("");
 const phone = ref("");
 const address = ref("");
+const memo = ref("");
 
 const form = useForm({
     file1: "",
@@ -46,6 +47,7 @@ const submit = () => {
         file1: form.file1,
         file2: form.file2,
         file3: form.file3,
+        memo: memo.value,
     };
 
     // Inertia.jsを使って会員情報を登録
@@ -177,6 +179,7 @@ const submit = () => {
                                 class="py-2 px-3 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
                                 rows="6"
                                 placeholder="メモ・備考などがあればこちらに入力してください。"
+                                v-model="memo"
                             ></textarea>
                         </div>
                     </div>
